cdefac7c35583c00a91b30fab31fe84dfa2373e8
Carlos Contreras <cdca12mx@gmail.com>
2019-02-18 15:27:59 -0700

    Creación de proyecto

---
+++ src/registrousuarios/gui/RegistroUsuariosGUI.java
@@ -0,0 +1,13 @@
+package registrousuarios.gui;
+
+/**
+ *
+ * @author Carlos Contreras
+ */
+public class RegistroUsuariosGUI {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
